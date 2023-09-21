aa787c0e784cb8982f0717f5f04f5e7836c33f40
IAMPETERNw <nwanahpeter@gmail.com>
2023-09-21 01:30:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
     return (1);
 }
