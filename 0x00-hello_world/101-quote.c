0ec5f266a8f6f584520f7035a36a7c159408b898
IAMPETERNw <nwanahpeter@gmail.com>
2023-09-20 19:37:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,4 +9,4 @@ int main(void)
 {
     write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
-}
+}	
